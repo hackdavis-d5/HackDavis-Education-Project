682b8b2db4ae51ddda75fd2b8029dc7d95cc33ac
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2019-02-09 20:49:38 -0800

    Hack together some Socket.IO code just to get something working.

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@socketio.on('my event')
+def test_message(message):
+    emit('my response', {'data': 'got it!'})
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
