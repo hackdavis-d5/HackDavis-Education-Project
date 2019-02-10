7d7a171fd4158632f4e0a7df3fe674bbadddd9aa
Sukhraj Sekhon <sukhraj5@mail.fresnostate.edu>
2019-02-09 23:15:32 -0800

    Fix upload issue

--- util/text_detection.py
+++ util/text_detection.py
@@ -8,139 +8,170 @@ import argparse
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
+def data_uri_to_cv2_img(uri):
+    encoded_data = uri.split(',')[1]
+    nparr = np.fromstring(encoded_data.decode('base64'), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-	help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-	help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
-	help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
-	help="resized image height (should be multiple of 32)")
-args = vars(ap.parse_args())
 
-# Sample input
-# python text_detection.py --image images/sign.jpg --east frozen_east_text_detection.pb
 
-# load the input image and grab the image dimensions
+def segmentImage(uri, confidence = .5):
+	# construct the argument parser and parse the arguments
+	# ap = argparse.ArgumentParser()
+	# ap.add_argument("-i", "--image", type=str,
+	# 	help="path to input image")
+	# ap.add_argument("-east", "--east", type=str,
+	# 	help="path to input EAST text detector")
+	# ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+	# 	help="minimum probability required to inspect a region")
+	# ap.add_argument("-w", "--width", type=int, default=320,
+	# 	help="resized image width (should be multiple of 32)")
+	# ap.add_argument("-e", "--height", type=int, default=320,
+	# 	help="resized image height (should be multiple of 32)")
+	# args = vars(ap.parse_args())
 
-# TODO: Pass in image instead of reading in from args
-image = cv2.imread(args["image"])
-orig = image.copy()
-(H, W) = image.shape[:2]
+	# load the input image and grab the image dimensions
+		
+	image = data_uri_to_cv2_img(uri)
 
-# set the new width and height and then determine the ratio in change
-# for both the width and height
-(newW, newH) = (args["width"], args["height"])
-rW = W / float(newW)
-rH = H / float(newH)
-
-# resize the image and grab the new image dimensions
-image = cv2.resize(image, (newW, newH))
-(H, W) = image.shape[:2]
-
-# define the two output layer names for the EAST detector model that
-# we are interested -- the first is the output probabilities and the
-# second can be used to derive the bounding box coordinates of text
-layerNames = [
-	"feature_fusion/Conv_7/Sigmoid",
-	"feature_fusion/concat_3"]
-
-# load the pre-trained EAST text detector
-print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
-
-# construct a blob from the image and then perform a forward pass of
-# the model to obtain the two output layer sets
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-	(123.68, 116.78, 103.94), swapRB=True, crop=False)
-start = time.time()
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
-end = time.time()
-
-# show timing information on text prediction
-print("[INFO] text detection took {:.6f} seconds".format(end - start))
-
-# grab the number of rows and columns from the scores volume, then
-# initialize our set of bounding box rectangles and corresponding
-# confidence scores
-(numRows, numCols) = scores.shape[2:4]
-rects = []
-confidences = []
-
-# loop over the number of rows
-for y in range(0, numRows):
-	# extract the scores (probabilities), followed by the geometrical
-	# data used to derive potential bounding box coordinates that
-	# surround text
-	scoresData = scores[0, 0, y]
-	xData0 = geometry[0, 0, y]
-	xData1 = geometry[0, 1, y]
-	xData2 = geometry[0, 2, y]
-	xData3 = geometry[0, 3, y]
-	anglesData = geometry[0, 4, y]
-
-	# loop over the number of columns
-	for x in range(0, numCols):
-		# if our score does not have sufficient probability, ignore it
-		if scoresData[x] < args["min_confidence"]:
-			continue
-
-		# compute the offset factor as our resulting feature maps will
-		# be 4x smaller than the input image
-		(offsetX, offsetY) = (x * 4.0, y * 4.0)
-
-		# extract the rotation angle for the prediction and then
-		# compute the sin and cosine
-		angle = anglesData[x]
-		cos = np.cos(angle)
-		sin = np.sin(angle)
-
-		# use the geometry volume to derive the width and height of
-		# the bounding box
-		h = xData0[x] + xData2[x]
-		w = xData1[x] + xData3[x]
-
-		# compute both the starting and ending (x, y)-coordinates for
-		# the text prediction bounding box
-		endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-		endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-		startX = int(endX - w)
-		startY = int(endY - h)
-
-		# add the bounding box coordinates and probability score to
-		# our respective lists
-		rects.append((startX, startY, endX, endY))
-		confidences.append(scoresData[x])
-
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
-boxes = non_max_suppression(np.array(rects), probs=confidences)
-#boxes.sort()
-images = []
-
-# loop over the bounding boxes
-c = 0
-for (startX, startY, endX, endY) in boxes:
-	# scale the bounding box coordinates based on the respective
-	# ratios
-	startX = int(startX * rW)
-	startY = int(startY * rH)
-	endX = int(endX * rW)
-	endY = int(endY * rH)
+	orig = image.copy()
 	
-	images.append(orig[startY:endY, startX:endX]) # Crop from {x, y, w, h } => {0, 0, 300, 400}
-	cv2.imshow("cropped" + str(c), orig[startY:endY, startX:endX])
-	cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-	c += 1
-	#cv2.waitKey(0)
-	# draw the bounding box on the image
-	
-# show the output image
-cv2.imshow("Text Detection", orig)
-cv2.waitKey(0)
+	(H, W) = image.shape[:2]
+
+	# set the new width and height and then determine the ratio in change
+	# for both the width and height
+	width_img = 320
+	height_img = 320
+	(newW, newH) = (width_img, height_img)
+	rW = W / float(newW)
+	rH = H / float(newH)
+
+	# resize the image and grab the new image dimensions
+	image = cv2.resize(image, (newW, newH))
+	(H, W) = image.shape[:2]
+
+	# define the two output layer names for the EAST detector model that
+	# we are interested -- the first is the output probabilities and the
+	# second can be used to derive the bounding box coordinates of text
+	layerNames = [
+		"feature_fusion/Conv_7/Sigmoid",
+		"feature_fusion/concat_3"]
+
+	# load the pre-trained EAST text detector
+	print("[INFO] loading EAST text detector...")
+	net = cv2.dnn.readNet(r"frozen_east_text_detection.pb")
+
+	# construct a blob from the image and then perform a forward pass of
+	# the model to obtain the two output layer sets
+	blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+		(123.68, 116.78, 103.94), swapRB=True, crop=False)
+	start = time.time()
+	net.setInput(blob)
+	(scores, geometry) = net.forward(layerNames)
+	end = time.time()
+
+	# show timing information on text prediction
+	print("[INFO] text detection took {:.6f} seconds".format(end - start))
+
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the geometrical
+		# data used to derive potential bounding box coordinates that
+		# surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability, ignore it
+			if scoresData[x] < confidence:
+				continue
+
+			# compute the offset factor as our resulting feature maps will
+			# be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+			# extract the rotation angle for the prediction and then
+			# compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+
+			# use the geometry volume to derive the width and height of
+			# the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+
+			# compute both the starting and ending (x, y)-coordinates for
+			# the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+
+			# add the bounding box coordinates and probability score to
+			# our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+
+	# apply non-maxima suppression to suppress weak, overlapping bounding
+	# boxes
+	boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+	# loop over the bounding boxes
+	for (startX, startY, endX, endY) in boxes:
+		# scale the bounding box coordinates based on the respective
+		# ratios
+		startX = int(startX * rW)
+		startY = int(startY * rH)
+		endX = int(endX * rW)
+		endY = int(endY * rH)
+
+		# draw the bounding box on the image
+		cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+	# show the output image
+	cv2.imshow("Text Detection", orig)
+	cv2.waitKey(0)
+
+
+def detect_document_uri(uri):
+    """Detects document features in the file located in Google Cloud
+    Storage."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.document_text_detection(image=image)
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
